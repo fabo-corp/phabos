1cd716d6164d2f363489d2fbe8f593f58c224c90
Fabien Parent <parent.f@gmail.com>
2014-12-06 16:19:54 +0800

    arm: arm-v7m: add support for atomic operations
    Add support for atomic operations on arm-v7m.
    
    Signed-off-by: Fabien Parent <parent.f@gmail.com>

---
+++ arch/arm/include/asm/atomic.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014-2015 Fabien Parent. All rights reserved.
+ * Author: Fabien Parent <parent.f@gmail.com>
+ *
+ * Provided under the three clause BSD license found in the LICENSE file.
+ */
+
+#ifndef __ATOMIC_H__
+#define __ATOMIC_H__
+
+#include <stdint.h>
+
+typedef int atomic_t;
+
+static inline uint32_t atomic_get(atomic_t *atomic)
+{
+    return *(uint32_t*) atomic;
+}
+
+static inline void atomic_init(atomic_t *atomic, uint32_t val)
+{
+    *atomic = (atomic_t) val;
+}
+
+uint32_t atomic_add(atomic_t *atomic, int n);
+uint32_t atomic_inc(atomic_t *atomic);
+uint32_t atomic_dec(atomic_t *atomic);
+
+#endif /* __ATOMIC_H__ */
+
