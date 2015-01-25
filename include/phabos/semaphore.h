8ecbced6b4edb8a3c84b7268283b2e2409b850e9
Fabien Parent <parent.f@gmail.com>
2015-01-25 21:15:16 +0100

    lib: add semaphore api
    Signed-off-by: Fabien Parent <parent.f@gmail.com>

---
+++ include/phabos/semaphore.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014-2015 Fabien Parent. All rights reserved.
+ * Author: Fabien Parent <parent.f@gmail.com>
+ *
+ * Provided under the three clause BSD license found in the LICENSE file.
+ */
+
+#ifndef __SEMAPHORE_H__
+#define __SEMAPHORE_H__
+
+#include <asm/atomic.h>
+#include <phabos/list.h>
+
+struct semaphore {
+    struct list_head wait_list;
+    atomic_t count;
+};
+
+struct semaphore *semaphore_create(unsigned val);
+void semaphore_init(struct semaphore *semaphore, unsigned val);
+void semaphore_lock(struct semaphore *semaphore);
+bool semaphore_trylock(struct semaphore *semaphore);
+void semaphore_unlock(struct semaphore *semaphore);
+void semaphore_destroy(struct semaphore *semaphore);
+
+#endif /* __SEMAPHORE_H__ */
+
---
+++ lib/semaphore.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2014-2015 Fabien Parent. All rights reserved.
+ * Author: Fabien Parent <parent.f@gmail.com>
+ *
+ * Provided under the three clause BSD license found in the LICENSE file.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <phabos/semaphore.h>
+#include <phabos/list.h>
+#include <phabos/scheduler.h>
+#include <phabos/assert.h>
+#include <asm/irq.h>
+
+struct semaphore *semaphore_create(unsigned val)
+{
+    struct semaphore *semaphore;
+
+    semaphore = malloc(sizeof(*semaphore));
+    if (!semaphore)
+        return NULL;
+    semaphore_init(semaphore, val);
+
+    return semaphore;
+}
+
+void semaphore_init(struct semaphore *semaphore, unsigned val)
+{
+    RET_IF_FAIL(semaphore,);
+
+    memset(semaphore, 0, sizeof(*semaphore));
+    list_init(&semaphore->wait_list);
+    atomic_init(&semaphore->count, val);
+}
+
+void semaphore_destroy(struct semaphore *semaphore)
+{
+    if (!semaphore)
+        return;
+
+    RET_IF_FAIL(list_is_empty(&semaphore->wait_list),);
+    free(semaphore);
+}
+
+void semaphore_lock(struct semaphore *semaphore)
+{
+    RET_IF_FAIL(semaphore,);
+
+    irq_disable();
+    while (atomic_get(&semaphore->count) <= 0) {
+        task_add_to_wait_list(task_get_running(), &semaphore->wait_list);
+        irq_enable();
+        task_yield();
+        irq_disable();
+    }
+
+    atomic_dec(&semaphore->count);
+    irq_enable();
+}
+
+bool semaphore_trylock(struct semaphore *semaphore)
+{
+    RET_IF_FAIL(semaphore, false);
+
+    irq_disable();
+    if (atomic_get(&semaphore->count) <= 0) {
+        irq_enable();
+        return false;
+    }
+
+    semaphore_lock(semaphore);
+    irq_enable();
+    return true;
+}
+
+void semaphore_unlock(struct semaphore *semaphore)
+{
+    RET_IF_FAIL(semaphore,);
+
+    atomic_inc(&semaphore->count);
+
+    if (!list_is_empty(&semaphore->wait_list))
+        task_remove_from_wait_list(list_first_entry(&semaphore->wait_list,
+                                                    struct task, list));
+}
