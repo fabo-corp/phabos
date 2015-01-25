d83e43278138de3fbc14db4b9de7f2c77e84ed40
Fabien Parent <parent.f@gmail.com>
2015-01-25 17:20:23 +0100

    tsb: add lowio support
    Add basic support for lowios for tsb.
    
    Signed-off-by: Fabien Parent <parent.f@gmail.com>

---
+++ arch/arm/mach-tsb/lowio.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2014-2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include <asm/hwio.h>
+#include "chip.h"
+
+#define UART_RBR_THR_DLL            (UART_BASE + 0x0)
+#define UART_LSR                    (UART_BASE + 0x14)
+#define UART_USR                    (UART_BASE + 0x7c)
+
+#define UART_LSR_THRE               (1 << 5)
+#define UART_USR_RFNE               (1 << 3)
+
+void machine_lowputchar(char c)
+{
+    while ((read32(UART_LSR) & UART_LSR_THRE) != UART_LSR_THRE);
+    write32(UART_RBR_THR_DLL, c);
+}
+
+int machine_lowgetchar(bool wait)
+{
+    if (!wait && (read32(UART_USR) & UART_USR_RFNE) == 0)
+        return EOF;
+
+    while ((read32(UART_USR) & UART_USR_RFNE) == 0);
+    return read32(UART_RBR_THR_DLL);
+}
