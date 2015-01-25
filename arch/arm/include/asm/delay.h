9c4843b0e2a3c0e1e4c5d4b46acbbcaaca484518
Fabien Parent <parent.f@gmail.com>
2015-01-25 18:31:20 +0100

    arm: add delay api
    Signed-off-by: Fabien Parent <parent.f@gmail.com>

---
+++ arch/arm/include/asm/delay.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2014-2015 Fabien Parent. All rights reserved.
+ * Author: Fabien Parent <parent.f@gmail.com>
+ *
+ * Provided under the three clause BSD license found in the LICENSE file.
+ */
+
+#ifndef __ARM_DELAY_H__
+#define __ARM_DELAY_H__
+
+#include <asm/machine.h>
+
+static inline void udelay(unsigned long usecs)
+{
+    for (int i = 0; i < LOOP_PER_USEC * usecs; i++)
+        asm volatile("nop");
+}
+
+static inline void mdelay(unsigned long msecs)
+{
+    udelay(msecs * 1000);
+}
+
+
+#endif /* __ARM_DELAY_H__ */
+
