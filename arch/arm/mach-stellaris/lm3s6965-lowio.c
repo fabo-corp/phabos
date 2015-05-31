c2f8e22a915b82f30b00fd7a576b0e09d48a6e96
Fabien Parent <parent.f@gmail.com>
2015-05-31 22:48:39 +0200

    stellaris: remove machine_lowgetchar support

--- arch/arm/mach-stellaris/lm3s6965-lowio.c
+++ arch/arm/mach-stellaris/lm3s6965-lowio.c
@@ -12,20 +12,8 @@
 
 #define UART_BASE   0x4000c000
 #define UARTDR      (UART_BASE + 0x0)
-#define UARTFR      (UART_BASE + 0x18)
-
-#define UARTFR_RXFE (1 << 4)
 
 void machine_lowputchar(char c)
 {
     write32(UARTDR, c);
-}
-
-int machine_lowgetchar(bool wait)
-{
-    if (!wait && read32(UARTFR) & UARTFR_RXFE)
-        return EOF;
-
-    while (read32(UARTFR) & UARTFR_RXFE);
-    return read32(UARTDR);
 }
