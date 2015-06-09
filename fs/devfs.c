c755030a7a675db134d86c28ab7410f1fc516cc7
Fabien Parent <parent.f@gmail.com>
2015-06-09 19:21:29 +0200

    fs: add devfs support

---
+++ fs/devfs.c
@@ -0,0 +1,52 @@
+#include <config.h>
+#include <phabos/hashtable.h>
+#include <phabos/utils.h>
+#include <phabos/assert.h>
+#include <phabos/fs.h>
+#include "ramfs.h"
+
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+static struct inode devfs_root = {
+    .flags = S_IFDIR,
+};
+
+static int devfs_mount(struct inode *cwd)
+{
+    RET_IF_FAIL(cwd, -EINVAL);
+    RET_IF_FAIL(is_directory(cwd), -EINVAL);
+
+    cwd->inode = devfs_root.inode;
+
+    return 0;
+}
+
+int devfs_init(void)
+{
+    return ramfs_mount(&devfs_root);
+}
+
+int devfs_mknod(const char *name, mode_t mode, dev_t dev)
+{
+    return ramfs_mknod(&devfs_root, name, mode, dev);
+}
+
+struct fs devfs_fs = {
+    .name = "devfs",
+
+    .file_ops = {
+        .getdents = ramfs_getdents,
+        .write = ramfs_write,
+        .read = ramfs_read,
+    },
+
+    .inode_ops = {
+        .mount = devfs_mount,
+        .mkdir = ramfs_mkdir,
+        .lookup = ramfs_lookup,
+        .mknod = ramfs_mknod,
+    },
+};
