81eddb1f87bb6b4bfff32d4ae07dbf2a4473f3c2
Fabien Parent <parent.f@gmail.com>
2015-07-22 01:43:43 +0200

    fixup! greybus: ap: add ap code

--- drivers/greybus/ap/ap.c
+++ drivers/greybus/ap/ap.c
@@ -1,41 +1,47 @@
 #include <phabos/utils.h>
 #include <phabos/assert.h>
-#include <phabos/usb/hcd.h>
-#include <phabos/usb/std-requests.h>
-#include <phabos/usb/driver.h>
+#include <phabos/greybus.h>
 
-void print_descriptor(void *raw_descriptor);
+#include "control-gb.h"
+#include "gpio-gb.h"
 
-static int gb_ap_init_bus(struct usb_device *dev)
+static void connect_gpio_cport(void)
 {
-    void *buffer = malloc(255);
-    struct usb_device_descriptor *desc = buffer;
+    struct gb_control_connected_request *req;
 
-    usb_control_msg(dev, USB_DEVICE_GET_DESCRIPTOR,
-                    USB_DESCRIPTOR_DEVICE << 8, 0, sizeof(*desc), desc);
+    struct gb_operation *operation = gb_operation_create(0, GB_CONTROL_TYPE_CONNECTED, sizeof(*req));
+    if (!operation)
+        return;
 
-    if (desc->idVendor != 0xffff)
-        return -EINVAL;
+    req = gb_operation_get_request_payload(operation);
+    req->cport_id = 3;
 
-    usb_control_msg(dev, USB_DEVICE_GET_DESCRIPTOR,
-                    USB_DESCRIPTOR_CONFIGURATION << 8, 0, 255, buffer);
+    gb_operation_send_request(operation, NULL, true);
+    gb_operation_destroy(operation);
+}
 
-    print_descriptor(buffer);
+static void toggle_gpio(void)
+{
+    struct gb_gpio_direction_out_request *req;
 
+    struct gb_operation *operation = gb_operation_create(1, GB_GPIO_TYPE_DIRECTION_OUT, sizeof(*req));
+    if (!operation)
+        return;
+
+    req = gb_operation_get_request_payload(operation);
+    req->which = 0;
+    req->value = 1;
+
+    gb_operation_send_request(operation, NULL, false);
+    gb_operation_destroy(operation);
+}
+
+int gb_ap_init(void)
+{
+    
+
+    connect_gpio_cport();
+    connect_gpio_cport();
+    toggle_gpio();
     return 0;
 }
-
-static struct usb_class_driver hub_class_driver = {
-    .class = 0,
-    .init = gb_ap_init_bus,
-};
-
-static int gb_ap_usb_init(struct driver *driver)
-{
-    return usb_register_class_driver(&hub_class_driver);
-}
-
-__driver__ struct driver gb_ap_usb_driver = {
-    .name = "gb-ap-usb",
-    .init = gb_ap_usb_init,
-};
