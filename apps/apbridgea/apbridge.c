ecf5013fcd850bb57436b93010ecda0ce906bd6b
Fabien Parent <parent.f@gmail.com>
2015-07-22 02:47:26 +0200

    apps: add apbridgea app

---
+++ apps/apbridgea/apbridge.c
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2014-2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <phabos/usb/gadget/gb-ap-bridge.h>
+#include <phabos/greybus.h>
+#include <phabos/scheduler.h>
+
+#include "apbridge_backend.h"
+#include "utils.h"
+
+#define IID_LENGTH 7
+
+// XXX phabos porting
+#define gb_dump(x...)
+
+static struct apbridge_dev_s *g_usbdev = NULL;
+static struct task *g_svc_thread;
+static struct apbridge_backend apbridge_backend;
+
+static int release_buffer(int status, const void *buf, void *priv)
+{
+    return usb_release_buffer(priv, buf);
+}
+
+static int usb_to_unipro(struct apbridge_dev_s *dev, void *buf, size_t len)
+{
+    struct gb_operation_hdr *hdr = buf;
+    unsigned int cportid;
+
+    gb_dump(buf, len);
+
+    if (len < sizeof(*hdr))
+        return -EPROTO;
+
+    /*
+     * Retreive and clear the cport id stored in the header pad bytes.
+     */
+    cportid = hdr->pad[1] << 8 | hdr->pad[0];
+    hdr->pad[0] = 0;
+    hdr->pad[1] = 0;
+
+    return apbridge_backend.usb_to_unipro(cportid, buf, len,
+                                          release_buffer, dev);
+}
+
+static int usb_to_svc(struct apbridge_dev_s *dev, void *buf, size_t len)
+{
+    gb_dump(buf, len);
+
+    return apbridge_backend.usb_to_svc(buf, len);
+}
+
+static int recv_from_svc(void *buf, size_t len)
+{
+    gb_dump(buf, len);
+
+    return svc_to_usb(g_usbdev, buf, len);
+}
+
+int recv_from_unipro(unsigned int cportid, void *buf, size_t len)
+{
+    struct gb_operation_hdr *hdr = (void *)buf;
+
+    /*
+     * FIXME: Remove when UniPro driver provides the actual buffer length.
+     */
+    len = gb_packet_size(buf);
+
+    gb_dump(buf, len);
+
+    /* Store the cport id in the header pad bytes (if we have a header). */
+    if (len >= sizeof(*hdr)) {
+        hdr->pad[0] = cportid & 0xff;
+        hdr->pad[1] = (cportid >> 8) & 0xff;
+    }
+
+    return unipro_to_usb(g_usbdev, buf, len);
+}
+
+static void manifest_event(unsigned char *manifest_file,
+                           int device_id, int manifest_number)
+{
+    char iid[IID_LENGTH];
+
+    snprintf(iid, IID_LENGTH, "IID-%d", manifest_number + 1);
+    printf("send manifest %d\n", manifest_number);
+    send_svc_event(0, iid, manifest_file);
+}
+
+static void svc_sim_fn(void *p_data)
+{
+    struct apbridge_dev_s *priv = p_data;
+
+    usb_wait(priv);
+    apbridge_backend.init();
+    send_svc_handshake();
+    send_ap_id(0);
+
+    foreach_manifest(manifest_event);
+}
+
+static int svc_sim_init(struct apbridge_dev_s *priv)
+{
+    g_usbdev = priv;
+    g_svc_thread = task_run(svc_sim_fn, priv, 0);
+    return g_svc_thread ? 0 : -1;
+}
+
+static struct apbridge_usb_driver usb_driver = {
+    .usb_to_unipro = usb_to_unipro,
+    .usb_to_svc = usb_to_svc,
+    .init = svc_sim_init,
+};
+
+int bridge_main(int argc, char *argv[])
+{
+    svc_register(recv_from_svc);
+    apbridge_backend_register(&apbridge_backend);
+    usbdev_apbinitialize(&usb_driver);
+
+    return 0;
+}
---
+++ apps/apbridgea/manifest.c
@@ -0,0 +1,316 @@
+/*
+ * Copyright (c) 2014-2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#include <asm/byteordering.h>
+
+#include "svc-emu.h"
+#include "manifest.h"
+
+// XXX phabos porting
+#define gb_debug(x...)
+#define gb_info(x...)
+#define gb_error(x...)
+#define ALIGN(x) (x + 3) & ~3
+
+struct greybus {
+    struct list_head cports;
+    struct greybus_driver *drv;
+};
+
+struct greybus g_greybus = {
+    .cports = { &g_greybus.cports, &g_greybus.cports}
+};
+
+struct manifest_file manifest_files[] = { {1, NULL}, }; // FIXME port
+static int g_device_id;
+
+void foreach_manifest(manifest_handler handler)
+{
+    int i = 0;
+    for (i = 0; i < ARRAY_SIZE(manifest_files); i++)
+        handler(manifest_files[i].bin, manifest_files[i].id, i);
+}
+
+static void *alloc_cport(void)
+{
+    struct gb_cport *gb_cport;
+
+    gb_cport = malloc(sizeof(struct gb_cport));
+    if (!gb_cport)
+        return NULL;
+
+    list_add(&g_greybus.cports, &gb_cport->list);
+    return gb_cport;
+}
+
+static void free_cport(int cportid)
+{
+    struct gb_cport *gb_cport;
+
+    list_foreach_safe(&g_greybus.cports, iter) {
+        gb_cport = list_entry(iter, struct gb_cport, list);
+        if (gb_cport->id == cportid) {
+            list_del(iter);
+            free(gb_cport);
+        }
+    }
+}
+
+/*
+ * Validate the given descriptor.  Its reported size must fit within
+ * the number of bytes reamining, and it must have a recognized
+ * type.  Check that the reported size is at least as big as what
+ * we expect to see.  (It could be bigger, perhaps for a new version
+ * of the format.)
+ *
+ * Returns the number of bytes consumed by the descriptor, or a
+ * negative errno.
+ */
+static int identify_descriptor(struct greybus_descriptor *desc, size_t size,
+                               int release)
+{
+    struct greybus_descriptor_header *desc_header = &desc->header;
+    size_t expected_size;
+    size_t desc_size;
+    struct gb_cport *cport;
+
+    if (size < sizeof(*desc_header)) {
+        gb_error("manifest too small\n");
+        return -EINVAL;         /* Must at least have header */
+    }
+
+    desc_size = (int)le16toh(desc_header->size);
+    if ((size_t) desc_size > size) {
+        gb_error("descriptor too big\n");
+        return -EINVAL;
+    }
+
+    /* Descriptor needs to at least have a header */
+    expected_size = sizeof(*desc_header);
+
+    switch (desc_header->type) {
+    case GREYBUS_TYPE_STRING:
+        expected_size += sizeof(struct greybus_descriptor_string);
+        expected_size += desc->string.length;
+
+        /* String descriptors are padded to 4 byte boundaries */
+        expected_size = ALIGN(expected_size);
+        break;
+    case GREYBUS_TYPE_INTERFACE:
+        expected_size += sizeof(struct greybus_descriptor_interface);
+        break;
+    case GREYBUS_TYPE_BUNDLE:
+        expected_size += sizeof(struct greybus_descriptor_bundle);
+        break;
+    case GREYBUS_TYPE_CPORT:
+        expected_size += sizeof(struct greybus_descriptor_cport);
+        if (desc_size >= expected_size) {
+            if (!release) {
+                cport = alloc_cport();
+                cport->id = desc->cport.id;
+                cport->protocol = desc->cport.protocol_id;
+                cport->device_id = g_device_id;
+                gb_debug("cport_id = %d\n", cport->id);
+            } else {
+                free_cport(desc->cport.id);
+            }
+        }
+        break;
+    case GREYBUS_TYPE_INVALID:
+    default:
+        gb_error("invalid descriptor type (%hhu)\n", desc_header->type);
+        return -EINVAL;
+    }
+
+    if (desc_size < expected_size) {
+        gb_error("%d: descriptor too small (%zu < %zu)\n",
+                 desc_header->type, desc_size, expected_size);
+        return -EINVAL;
+    }
+
+    /* Descriptor bigger than what we expect */
+    if (desc_size > expected_size) {
+        gb_error("%d descriptor size mismatch (want %zu got %zu)\n",
+                 desc_header->type, expected_size, desc_size);
+    }
+
+    return desc_size;
+}
+
+bool _manifest_parse(void *data, size_t size, int release)
+{
+    struct greybus_manifest *manifest = data;
+    struct greybus_manifest_header *header = &manifest->header;
+    struct greybus_descriptor *desc;
+    uint16_t manifest_size;
+
+    if (!release) {
+        /* we have to have at _least_ the manifest header */
+        if (size <= sizeof(manifest->header)) {
+            gb_error("short manifest (%zu)\n", size);
+            return false;
+        }
+
+        /* Make sure the size is right */
+        manifest_size = le16toh(header->size);
+        if (manifest_size != size) {
+            gb_error("manifest size mismatch %zu != %hu\n", size,
+                     manifest_size);
+            return false;
+        }
+
+        /* Validate major/minor number */
+        if (header->version_major > GREYBUS_VERSION_MAJOR) {
+            gb_error("manifest version too new (%hhu.%hhu > %hhu.%hhu)\n",
+                     header->version_major, header->version_minor,
+                     GREYBUS_VERSION_MAJOR, GREYBUS_VERSION_MINOR);
+            return false;
+        }
+    }
+
+    /* OK, find all the descriptors */
+    desc = (struct greybus_descriptor *)(header + 1);
+    size -= sizeof(*header);
+    while (size) {
+        int desc_size;
+
+        desc_size = identify_descriptor(desc, size, release);
+        if (desc_size <= 0)
+            return false;
+        desc = (struct greybus_descriptor *)((char *)desc + desc_size);
+        size -= desc_size;
+    }
+
+    return true;
+}
+
+/*
+ * Parse a buffer containing a interface manifest.
+ *
+ * If we find anything wrong with the content/format of the buffer
+ * we reject it.
+ *
+ * The first requirement is that the manifest's version is
+ * one we can parse.
+ *
+ * We make an initial pass through the buffer and identify all of
+ * the descriptors it contains, keeping track for each its type
+ * and the location size of its data in the buffer.
+ *
+ * Returns true if parsing was successful, false otherwise.
+ */
+bool manifest_parse(void *data, size_t size)
+{
+    return _manifest_parse(data, size, 0);
+}
+
+bool manifest_release(void *data, size_t size)
+{
+    return _manifest_parse(data, size, 1);
+}
+
+static int get_interface_id(char *fname)
+{
+    char *iid_str;
+    int iid = 0;
+    char tmp[256];
+
+    strcpy(tmp, fname);
+    iid_str = strtok(tmp, "-");
+    if (!strncmp(iid_str, "IID", 3))
+        iid = strtol(fname + 4, NULL, 0);
+
+    return iid;
+}
+
+void *get_manifest_blob(void)
+{
+    return manifest_files[0].bin;
+}
+
+void parse_manifest_blob(void *manifest)
+{
+    struct greybus_manifest_header *mh = manifest;
+
+    manifest_parse(mh, le16toh(mh->size));
+}
+
+void release_manifest_blob(void *manifest)
+{
+    struct greybus_manifest_header *mh = manifest;
+
+    manifest_release(mh, le16toh(mh->size));
+}
+
+void enable_manifest(char *name, void *priv, int device_id)
+{
+    void *manifest;
+
+    manifest = get_manifest_blob();
+    if (manifest) {
+        g_device_id = device_id;
+        parse_manifest_blob(manifest);
+        int iid = get_interface_id(name);
+        if (iid > 0) {
+            gb_info("%s interface inserted\n", name);
+        } else {
+            gb_error("invalid interface ID, no hotplug plug event sent\n");
+        }
+    } else {
+        gb_error("missing manifest blob, no hotplug event sent\n");
+    }
+}
+
+void disable_manifest(char *name, void *priv, int device_id)
+{
+    void *manifest;
+
+    manifest = get_manifest_blob();
+    if (manifest) {
+        g_device_id = device_id;
+        release_manifest_blob(manifest);
+    }
+}
+
+struct list_head *get_manifest_cports(void)
+{
+    return &g_greybus.cports;
+}
+
+int get_manifest_size(void)
+{
+    struct greybus_manifest_header *mh = get_manifest_blob();
+
+    return mh ? le16_to_cpu(mh->size) : 0;
+}
