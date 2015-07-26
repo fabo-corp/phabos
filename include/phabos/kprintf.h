5e3457f991c175eb38f3acfade6643b4fe6e8c50
Fabien Parent <parent.f@gmail.com>
2015-07-27 01:43:26 +0200

    lib: export kvprintf prototype

--- include/phabos/kprintf.h
+++ include/phabos/kprintf.h
@@ -8,9 +8,12 @@
 #ifndef __KPRINTF_H__
 #define __KPRINTF_H__
 
+#include <stdarg.h>
+
 void kputc(char c);
 int kputs(const char* str);
 int kprintf(const char* format, ...) __attribute__((format(printf, 1, 2)));
+int kvprintf(const char *format, va_list ap);
 
 #endif /* __KPRINTF_H__ */
 
