d2899dc5012559ed8cec17cfd7a9ad1287550f0a
Fabien Parent <parent.f@gmail.com>
2015-08-03 02:29:51 +0200

    stm32: clean-up GPIO driver

--- arch/arm/mach-stm32/gpio.c
+++ arch/arm/mach-stm32/gpio.c
@@ -1,34 +1,52 @@
 #include <asm/gpio.h>
 #include <asm/hwio.h>
 
-#define GPIO_MODER_INPUT        0
-#define GPIO_MODER_OUTPUT       1
-#define GPIO_MODER_ALT_FUNCTION 2 
-
 #define GPIO_BASE   0x40020000
 #define GPIO_SIZE   0x400
 
-#define GPIO_MODER   0x00
-#define GPIO_OTYPER  0x04
-#define GPIO_PUPDR   0x0c
-#define GPIO_BSRR    0x18
-#define GPIO_AFRL    0x20
+#define GPIO_MODER      0x00
+#define GPIO_OTYPER     0x04
+#define GPIO_OSPEEDR    0x08
+#define GPIO_PUPDR      0x0c
+#define GPIO_BSRR       0x18
+#define GPIO_AFRL       0x20
+#define GPIO_AFRH       0x24
+
+static inline unsigned config_to_port(unsigned long config)
+{
+    return (config >> GPIO_PORT_OFFSET) & GPIO_PORT_MASK;
+}
 
 static inline uintptr_t config_to_regbase(unsigned long config)
 {
-    return GPIO_BASE + ((config >> 4) & 0xff) * 0x400;
+    return GPIO_BASE + config_to_port(config) * GPIO_SIZE;
 }
 
 static inline int config_to_pin(unsigned long config)
 {
-    return config & 0xf;
+    return (config >> GPIO_PIN_OFFSET) & GPIO_PIN_MASK;
+}
+
+static unsigned config_to_af(unsigned long config)
+{
+    return (config >> GPIO_AF_OFFSET) & GPIO_AF_MASK;
+}
+
+static void gpio_update(uint32_t reg, unsigned offset, unsigned long mask,
+                        unsigned long config, unsigned long config_offset)
+{
+    uintptr_t base = config_to_regbase(config);
+
+    uint32_t tmp = read32(base + reg);
+    tmp &= ~(mask << offset);
+    tmp |= ((config >> config_offset) & mask) << offset;
+    write32(base + reg, tmp);
 }
 
 int stm32_configgpio(unsigned long config)
 {
-    uintptr_t base = config_to_regbase(config);
     int pin = config_to_pin(config);
-    uint32_t moder;
+    uint32_t mode = config & (GPIO_MODE_MASK << GPIO_MODE_OFFSET);
 
     if (config & GPIO_OUTPUT_CLEAR)
         stm32_gpiowrite(config, false);
@@ -36,26 +54,25 @@ int stm32_configgpio(unsigned long config)
     if (config & GPIO_OUTPUT_SET)
         stm32_gpiowrite(config, true);
 
-    moder = read32(base + GPIO_MODER);
-    moder &= ~(3 << (pin << 1));
+    gpio_update(GPIO_OTYPER, pin, GPIO_TYPE_MASK, config, GPIO_TYPE_OFFSET);
+    gpio_update(GPIO_PUPDR, pin << 1, GPIO_PUSHPULL_MASK, config,
+                GPIO_PUSHPULL_OFFSET);
+    gpio_update(GPIO_OSPEEDR, pin << 1, GPIO_SPEED_MASK, config,
+                GPIO_SPEED_OFFSET);
 
-    if (config & GPIO_OUTPUT)
-        moder |= GPIO_MODER_OUTPUT << (pin << 1);
+    if (mode == GPIO_ALT_FCT) {
+        unsigned af = config_to_af(config);
 
-    write32(base + GPIO_MODER, moder);
-
-    if (config & GPIO_OPENDRAIN) {
-        read32(base + GPIO_OTYPER) |= 1 << pin;
-    } else {
-        read32(base + GPIO_OTYPER) &= ~(1 << pin);
+        if (af < 8) {
+            gpio_update(GPIO_AFRL, pin << 2, GPIO_AF_MASK, config,
+                        GPIO_AF_OFFSET);
+        } else {
+            gpio_update(GPIO_AFRH, (pin - 8) << 2, GPIO_AF_MASK,
+                        config - (GPIO_AF8 << GPIO_AF_OFFSET), GPIO_AF_OFFSET);
+        }
     }
 
-    read32(base + GPIO_PUPDR) &= ~(3 << (pin << 1));
-
-    if ((config & GPIO_PULLDOWN) == GPIO_PULLDOWN)
-        read32(base + GPIO_PUPDR) |= GPIO_PULLDOWN << (pin << 1);
-    else if ((config & GPIO_PULLUP) == GPIO_PULLUP)
-        read32(base + GPIO_PUPDR) |= GPIO_PULLUP << (pin << 1);
+    gpio_update(GPIO_MODER, pin << 1, GPIO_MODE_MASK, config, GPIO_MODE_OFFSET);
 
     return 0;
 }
@@ -64,13 +81,10 @@ int stm32_gpiowrite(unsigned long port, bool high_level)
 {
     uintptr_t base = config_to_regbase(port);
 
-//    stm32_configgpio(port);
-
-    if (high_level) {
+    if (high_level)
         write32(base + GPIO_BSRR, 1 << config_to_pin(port));
-    } else {
+    else
         write32(base + GPIO_BSRR, (1 << config_to_pin(port)) << 16);
-    }
 
     return 0;
 }
--- arch/arm/mach-stm32/include/asm/gpio.h
+++ arch/arm/mach-stm32/include/asm/gpio.h
@@ -3,6 +3,33 @@
 
 #include <stdbool.h>
 
+/*
+ * config[20:20] = GPIO Output set
+ * config[19:19] = GPIO Output clear
+ * config[18:17] = Floating/Pull up/Pull down
+ * config[16:16] = Output type
+ * config[15:14] = Mode
+ * config[13:12] = Speed
+ * config[11:8]  = Alternate Function
+ * config[7:4]   = Port
+ * config[3:0]   = Pin
+ */
+#define GPIO_PIN_OFFSET         0
+#define GPIO_PORT_OFFSET        4
+#define GPIO_AF_OFFSET          8
+#define GPIO_SPEED_OFFSET       12
+#define GPIO_MODE_OFFSET        14
+#define GPIO_TYPE_OFFSET        16
+#define GPIO_PUSHPULL_OFFSET    17
+
+#define GPIO_PIN_MASK           0xf
+#define GPIO_PORT_MASK          0xf
+#define GPIO_AF_MASK            0xf
+#define GPIO_SPEED_MASK         0x3
+#define GPIO_MODE_MASK          0x3
+#define GPIO_TYPE_MASK          0x1
+#define GPIO_PUSHPULL_MASK      0x3
+
 #define GPIO_PORTA  (0 << 4)
 #define GPIO_PORTB  (1 << 4)
 #define GPIO_PORTC  (2 << 4)
@@ -12,6 +39,23 @@
 #define GPIO_PORTG  (6 << 4)
 #define GPIO_PORTH  (7 << 4)
 #define GPIO_PORTI  (8 << 4)
+
+#define GPIO_AF0    (0 << 8)
+#define GPIO_AF1    (1 << 8)
+#define GPIO_AF2    (2 << 8)
+#define GPIO_AF3    (3 << 8)
+#define GPIO_AF4    (4 << 8)
+#define GPIO_AF5    (5 << 8)
+#define GPIO_AF6    (6 << 8)
+#define GPIO_AF7    (7 << 8)
+#define GPIO_AF8    (8 << 8)
+#define GPIO_AF9    (9 << 8)
+#define GPIO_AF10   (10 << 8)
+#define GPIO_AF11   (11 << 8)
+#define GPIO_AF12   (12 << 8)
+#define GPIO_AF13   (13 << 8)
+#define GPIO_AF14   (14 << 8)
+#define GPIO_AF15   (15 << 8)
 
 #define GPIO_PIN0   (0 << 0)
 #define GPIO_PIN1   (1 << 0)
@@ -30,18 +74,24 @@
 #define GPIO_PIN14  (14 << 0)
 #define GPIO_PIN15  (15 << 0)
 
-#define GPIO_INPUT      (0 << 16)
-#define GPIO_OUTPUT     (1 << 16)
+#define GPIO_SPEED_LOW      (0 << 12)
+#define GPIO_SPEED_MEDIUM   (1 << 12)
+#define GPIO_SPEED_FAST     (2 << 12)
+#define GPIO_SPEED_HIGH     (3 << 12)
 
-#define GPIO_OUTPUT_CLEAR (1 << 21)
-#define GPIO_OUTPUT_SET   (1 << 17)
+#define GPIO_INPUT      (0 << 14)
+#define GPIO_OUTPUT     (1 << 14)
+#define GPIO_ALT_FCT    (2 << 14)
 
-#define GPIO_PUSHPULL   (0 << 18)
-#define GPIO_OPENDRAIN  (1 << 18)
+#define GPIO_OUTPUT_CLEAR (1 << 18)
+#define GPIO_OUTPUT_SET   (1 << 19)
 
-#define GPIO_FLOAT      (0 << 19)
-#define GPIO_PULLUP     (1 << 19)
-#define GPIO_PULLDOWN   (2 << 19)
+#define GPIO_PUSHPULL   (0 << 16)
+#define GPIO_OPENDRAIN  (1 << 16)
+
+#define GPIO_FLOAT      (0 << 17)
+#define GPIO_PULLUP     (1 << 17)
+#define GPIO_PULLDOWN   (2 << 17)
 
 int stm32_configgpio(unsigned long config);
 int stm32_gpiowrite(unsigned long port, bool high_level);
