5368533d7c5acba55e44e20b7b3e555d463f2cfe
Fabien Parent <parent.f@gmail.com>
2015-08-10 01:25:17 +0200

    stm32: fix compil

--- arch/arm/mach-stm32/include/asm/machine.h
+++ arch/arm/mach-stm32/include/asm/machine.h
@@ -14,10 +14,12 @@
 #define HZ              100
 
 #if defined(CONFIG_BOOT_FLASH)
-#define USEC_PER_LOOP   25
+#define LOOP_PER_USEC   25
+#define LOOP_PER_MSEC   25000
 #define CPU_FREQ        160000000
 #else
-#define USEC_PER_LOOP   47
+#define LOOP_PER_USEC   47
+#define LOOP_PER_MSEC   47000
 #define CPU_FREQ        168000000
 #endif
 
