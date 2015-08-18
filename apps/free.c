bc1aa5552c85ab9d83b3e1a678345dd130fcf523
Fabien Parent <parent.f@gmail.com>
2015-08-18 02:13:44 +0200

    apps: add free application
    Add free application to get information about the current memory usage.
    
    Signed-off-by: Fabien Parent <parent.f@gmail.com>

---
+++ apps/free.c
@@ -0,0 +1,29 @@
+#include <phabos/kprintf.h>
+#include <phabos/mm.h>
+#include <apps/shell.h>
+
+static int free_main(int argc, char **argv)
+{
+    unsigned long total;
+    unsigned long used;
+    unsigned long cached;
+    unsigned long free;
+
+    struct mm_usage *usage = mm_get_usage();
+
+    total = (unsigned long) atomic_get(&usage->total);
+    used = (unsigned long) atomic_get(&usage->used);
+    cached = (unsigned long) atomic_get(&usage->cached);
+    free = total - used;
+
+    kprintf("      %12s %12s %12s %12s\n", "total", "used", "free", "cache");
+    kprintf("Mems: %12lu %12lu %12lu %12lu\n", total, used, free, cached);
+
+    return 0;
+}
+
+__shell_command__ struct shell_command free_command = {
+    .name = "free",
+    .description = "Get memory usage statistics",
+    .entry = free_main,
+};
